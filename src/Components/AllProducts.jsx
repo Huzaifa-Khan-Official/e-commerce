0c8a12e841e95bba285422f8a606f8623be8b56d
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-04-05 20:17:05 +0500

    product rendering done

--- src/Components/AllProducts.jsx
+++ src/Components/AllProducts.jsx
@@ -1,14 +1,37 @@
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function AllProducts() {
+    const [data, setData] = useState([]);
     useEffect(() => {
         axios.get('https://dummyjson.com/products')
-            .then(res => console.log(res))
+            .then(res => setData(res.data.products))
             .catch(err => console.log(err))
     }, [])
 
     return (
-        <div>AllProducts</div>
+        <div className='row justify-content-center mt-4 px-4 gap-4'>
+            {
+                data.map((singleProduct, index) => {
+                    return (
+                        <div key={index} className='cardBody col-md-5 col-12 rounded shadow mt-4'>
+                            <img src={singleProduct.thumbnail} className="productImage rounded" alt="" />
+                            <div className="cardHeading">
+                                <div>
+                                    <h3 className='productTitle'>{singleProduct.title}</h3>
+                                    <p className='productDesc'>{singleProduct.description}</p>
+                                    <p className="productRating">
+                                        {singleProduct.rating}
+                                    </p>
+                                    <p className="productPrice">
+                                        {singleProduct.price}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
     )
 }
